dad04d9fe93ebd0a820ece910dde92b40f2f2a8e
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2020-12-15 09:37:19 +0000

    UpdateComments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
-  def index
-  end
 
   def create
+    @map = Map.find(params[:map_id])
+    @comment = Comment.new(comment_params)
+    @comment.map_id = @map.id
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to request.referer
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
@@ -10,4 +17,17 @@ class CommentsController < ApplicationController
 
   def update
   end
+
+  def destroy
+    @map = Map.find(params[:id])
+    comment = @map.comments.find(params[:id])
+    comment.destroy
+    redirect_to request.referer
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :comment, :image)
+  end
+
 end
