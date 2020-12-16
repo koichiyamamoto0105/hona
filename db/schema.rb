1d3a0b249ed1667c79d8c282acdb41d6ae846101
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2020-12-16 10:24:19 +0000

    addstarcolumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_072720) do
+ActiveRecord::Schema.define(version: 2020_12_16_033409) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_072720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+    t.integer "star"
   end
 
   create_table "favorites", force: :cascade do |t|
