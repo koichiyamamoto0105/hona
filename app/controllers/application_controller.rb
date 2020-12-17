fb0b734ead642141a139d6c9ff68fd725c79ca87
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2020-12-17 08:31:23 +0000

    Updated i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+   I18n.locale = locale
+  end
+
+  def locale
+   @locale ||= params[:locale] ||= I18n.default_locale
+  end
+
+  def default_url_options(options={})
+   options.merge(locale: locale)
+  end
 
   protected
 
--- config/initializers/country_select.rb
+++ config/initializers/country_select.rb
@@ -1,3 +1,4 @@
 CountrySelect::FORMATS[:with_alpha2] = lambda do |country|
-  "#{country.translations[I18n.locale.to_s]} (#{country.alpha2})"
+  "#{country.translations[I18n.locale.to_s]} "
+  "#{country.name} (#{country.alpha2})"
 end
