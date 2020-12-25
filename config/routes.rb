0c1af36130a64f51cfb0942f9934c1c975e51407
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2020-12-25 10:12:57 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   get 'search/search'
   devise_for :users
-   scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
     root 'maps#top'
 
-    resources :users do
-        member do
-            get '/favorites' => 'users#favorites', as: 'favorites'
-        end
+    resources :users, except: [:index, :destroy] do
+      member do
+        get '/favorites' => 'users#favorites', as: 'favorites'
+      end
     end
     resources :maps do
-     resource :favorites, only: [:create, :destroy, :index]
-     resources :comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy, :index]
+      resources :comments, only: [:create, :destroy]
     end
-
+    # get '/map/hashtag/:name', to: "maps#hashtag"
     get '/search', to: 'search#search'
-
-
-
   end
 end
