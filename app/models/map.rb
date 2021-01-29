b6a16d46b1fcec4a0e20e2c0e214fd881ba2b776
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-01-29 13:00:42 +0000

    Ranking for country

--- app/models/map.rb
+++ app/models/map.rb
@@ -8,25 +8,32 @@ class Map < ApplicationRecord
   validates :longitude, presence: true, :uniqueness => { scope: [:spotname]}
   validates :spotname, presence: true, uniqueness: true
 
-  geocoded_by :address
-  before_validation :geocode
+  #geocoded_by :address
+  #before_validation :geocode
+
+  def country_name
+    c = ISO3166::Country[country]
+    c.translations[I18n.locale.to_s] || c.name
+  end
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
   def self.create_all_ranks
-   Map.find(Favorite.group(:map_id).order('count(map_id) desc').limit(3).pluck(:map_id))
+   Map.find(Favorite.group(:map_id).order(Arel.sql('count(map_id) desc')).limit(3).pluck(:map_id))
   end
 
-  # def self.create_country_ranks
-  #   results = {}
-  #   countries = User.group(:country).pluck(:country)
-  #   countries.each do |country|
-  #   result = Map.find(Favorite.joins(:user).where("users.country='#{country}'").group(:map_id).order('count(map_id) desc').limit(3).pluck(:map_id))
-  #   results[country] = result
-  # end
-  # end
+  def self.create_country_ranks
+    results = {}
+    countries = User.group(:country).pluck(:country)
+    countries.each do |country|
+    result = Map.find(Favorite.joins(:user).where("users.country='#{country}'").group(:map_id).order(Arel.sql('count(map_id) desc')).limit(3).pluck(:map_id))
+    # byebug
+    results[country] = result
+  end
+   return results
+  end
 
   def self.search_for(content, method)
     if method == 'perfect'
@@ -38,6 +45,11 @@ class Map < ApplicationRecord
     else
       Map.where('spotname LIKE ?', '%' + content + '%')
     end
+  end
+
+  def country_name
+    c = ISO3166::Country[country]
+    c.translations[I18n.locale.to_s] || c.name
   end
 
 end
