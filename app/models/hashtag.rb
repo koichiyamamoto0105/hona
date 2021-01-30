0d3164ca1abf8786b769f56358bd115424449f20
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-01-30 12:13:14 +0000

    Add Search def

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,4 +2,17 @@ class Hashtag < ApplicationRecord
   validates :hashname, presence: true, length: { maximum:50}
   has_many :hashtag_spots, dependent: :destroy
   has_many :comments, through: :hashtag_spots
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Hashtag.where(hashname: content)
+    elsif method == 'forward'
+      Hashtag.where('hashname LIKE ?', content+'%')
+    elsif method == 'backward'
+      Hashtag.where('hashname LIKE ?', '%'+content)
+    else
+      Hashtag.where('hashname LIKE ?', '%'+content+'%')
+    end
+  end
+
 end
