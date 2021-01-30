57f78345ce0a4d99d7ed401d0335909dbc0531c8
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-01-30 12:15:15 +0000

    Removed unnecessary Line

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,5 @@
 module CommentsHelper
   def render_with_hashtags(comment)
-
     comment.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/comment/hashtag/#{word.delete("#")}"}.html_safe
   end
 end
