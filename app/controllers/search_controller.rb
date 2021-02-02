c3fbb6560aedba5579fbc078b6fdbcf39bb1ddad
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-02-02 11:29:38 +0000

    Add Hashtag search def

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,10 @@ class SearchController < ApplicationController
     @model = params[:model]
     @content = params[:content]
     @method = params[:method]
-    @model = 'map'
-    @records = Map.search_for(@content, @method)
-    # if @model == 'map'
-    #   @records = Map.search_for(@content, @method)
-    # # else
-    #   @records = User.search_for(@content, @method)
-    # end
+    if @model == 'map'
+      @records = Map.search_for(@content, @method)
+    else
+      @records = Hashtag.search_for(@content, @method)
+    end
   end
 end
