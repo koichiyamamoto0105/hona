a44cbefd3f7f21a803b44c4962ca81ff22c8b249
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-02-05 13:02:38 +0000

    Hashtag index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
     @comment.map_id = @map.id
     @comment.user_id = current_user.id
     if @comment.save
+      # sleep(3)
       redirect_to map_path(@map.id)
     else
       @comments = @map.comments.page(params[:page]).per(3)
@@ -27,15 +28,30 @@ class CommentsController < ApplicationController
   end
 
   def hashtag
-
     @user = current_user
     @tag = Hashtag.find_by(hashname: params[:name])
     @comments = @tag.comments
   end
 
+  def all_hashtag
+    @user = current_user
+    if params[:name].nil?
+      @tags = Hashtag.all.to_a.group_by{ |hashtag| hashtag.comments.count}
+    else
+      @tag = Hashtag.find_by(hashname: params[:name])
+      @comments = @tag.comments.page(params[:page]).per(20).reverse_order
+      @tags = Hashtag.all.to_a.group_by{ |hashtag| hashtag.comments.count}
+    end
+  end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:title, :comment, :image, :star)
+    params.require(:comment).permit(:title, :comment, :image, :star, :image_id)
   end
+
+  def map_params
+    params.require(:map).permit(:address, :latitude, :longitude, :spotname)
+  end
+
 end
