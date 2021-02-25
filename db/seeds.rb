f7a770a3b9e365de944a5b1b24ac902d28f8bb5b
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-02-25 12:30:46 +0000

    Add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create!(
+#   email: 'lyon@test.com',
+#   name: 'pierrelyon',
+#   nickname: 'lyon',
+#   country: 'FR'
+#   )
+
+Map.create!(
+  address: '甲子園球場',
+  latitude: 34.721372200000005,
+  longitude: 135.36164729804025,
+  spotname: '甲子園球場',
+  user_id: 1
+  )
